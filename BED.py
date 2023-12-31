6d9d1445a9620cc21da5d43e4b88fe35c0364974
Allison Ng <ang@setonix-01.pawsey.org.au>
2023-12-31 10:30:22 +0800

    Updated code to calc dose per fraction from BED

--- BED.py
+++ BED.py
@@ -2,8 +2,12 @@ import numpy as np
 def get_equivalent_bed_treatment(param, D_BED_0, n):
     # Calculate BED_0
     #param2 is alpha, param3 is beta
-    BED_0 = (1 / (param[2] / param[3])) * D_BED_0[0] * (D_BED_0[1] ** 2) + D_BED_0[0] * D_BED_0[1]
-    print(BED_0)
+    #D_BED_0 either a list in form [n, d] or a number (which is desired BED)
+    if isinstance(D_BED_0, list):
+      BED_0 = 1 / (param[2] / param[3]) * D_BED_0[0] * (D_BED_0[1] ** 2) + D_BED_0[0] * D_BED_0[1]
+    else:
+      BED_0 = D_BED_0 
+
     m = n
 
     # Calculate the dose per fraction for each treatment given by the number of fractions and the equivalent BED
