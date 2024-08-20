9dfc2d2f43fe272ca410f879ea5472df5872f373
Allison Ng <23303755@student.uwa.edu.au>
2024-08-20 09:24:35 +0800

    debugged some variable assign/syntax errors

--- RTTreatmentEval.py
+++ RTTreatmentEval.py
@@ -1,29 +1,46 @@
-#measure time taken for algorithm
-import time
-start_time = time.time()
-#import relevant modules
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from differential_equations import radioimmuno_response_model
-import new_data_processing as dp
-from data_processing import getCellCounts
-from BED import get_equivalent_bed_treatment
-import concurrent.futures
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 11:47:21 2024
 
-#returns list of schedules and dose list
-def get_treatment_and_dose(bioEffDose, n, param):
-  schedule_list = get_treatment_schedules(n, 10)
+@author: allis
+"""
+
+# Import the necessary modules
+import numpy as np
+import concurrent.futures
+import new_data_processing as dp
+from differential_equations import radioimmuno_response_model
+from BED import get_equivalent_bed_treatment
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+start_time = time.time()
+# Define the number of patients
+num_patients = 10
+def get_treatment_and_dose(bioEffDose, numRT, param, numPD, numCTLA4):
+  RTschedule_list = get_treatment_schedules(numRT, 10)
+  if numPD > 0:
+    PDschedule_list = get_treatment_schedules(numPD, 10)
+  else:
+    PDschedule_list = []
+  if numCTLA4 > 0:
+      CTLA4schedule_list = get_treatment_schedules(numCTLA4, 10)
+  else:
+      CTLA4schedule_list = []
+  schedule = []
+  for x in RTschedule_list:
+    schedule.append(x)
   DList = []
-  D = get_equivalent_bed_treatment(param, bioEffDose, n)
-  for i in range(len(schedule_list)):
+  D = get_equivalent_bed_treatment(param, bioEffDose, numRT)
+  for i in range(len(schedule)):
     DList.append(D)
-  return schedule_list, DList
+  return schedule, DList
 
 #recursive function to obtain treatment schedules
 def get_treatment_schedules(n, start):
   schedule_list = []
   if n == 1:
+    #return [[x] for x in range(start, 16)]
     return [[x] for x in range(start, 31)]
   else:
 
@@ -39,58 +56,88 @@ dT = 0.98
 t_f1 = 0
 t_f2 = 50
 delta_t = 0.05
-t_treat_c4 = np.zeros(3)
-t_treat_p1 = np.zeros(3)
+# t_treat_c4 = np.zeros(3)
+# t_treat_p1 = np.zeros(3)
 c4 = 0
 p1 = 0
-errorControl = pd.read_csv("errors for control set.csv")
-errorControl = list(np.transpose(np.array(errorControl))[0])
-errorRT = pd.read_csv("errors for RT set.csv")
-errorRT = list(np.transpose(np.array(errorRT))[0])
-param = pd.read_csv("mean of each parameter for RT set.csv")
-param = tuple(np.transpose(np.array(param))[0])
-errors = [errorControl, errorRT]
-errorMerged = dp.merge_lists(errors)
-sample_size = 2000
+         #print('errors', errorMerged)
 all_res_list = []
-IT = (False, False)
-num_fractions = 1
-file_name = 'RT Mean Treatment Times ' + str(num_fractions) + ' fraction.csv'
-schedule_list, DList = get_treatment_and_dose(60, num_fractions, param)
-paramNew = list(param)
+IT = (True, True)
+RT_fractions = 1
+file_name = 'new RT ' + str(RT_fractions) + ' fraction a.csv'
+schedule_list, DList = get_treatment_and_dose(60, RT_fractions, param, 0, 0)
+#print('Dlist', DList)
+#print(schedule_list)
+# paramNew = list(param)
 
-def evaluate_patient(i, k):
-  for j in range(len(param)):
-      if errorMerged[j] != 0:
-        #gets log normal parameters
-        logNormalParams = dp.log_normal_parameters(param[j], errorMerged[j])
-        #samples parameters from log normal distribution
-        paramNew[j] = min(max(np.random.lognormal(mean=logNormalParams[0], sigma = logNormalParams[1]), 0.8*param[j]), 3*param[j])
-      if j == 26:
-        paramNew[j] = min(max(np.random.lognormal(mean=logNormalParams[0], sigma = logNormalParams[1]), 0.8*param[j]), 1.2*param[j])
-      if j in [22, 32] and IT == (False, False):
-        #ensure pd1 and ctla concentration is 0
-        paramNew[j] = 0
+# Generate a list of seeds
+
+# Assuming 'params' is your list of parameters
+# Load from CSV
+params = pd.read_csv('parameters.csv').values.tolist()
+sample_size = len(params)
+#print(params)
+# Now you have a list of parameters for each patient
+# You can now evaluate the treatment schedules in parallel
+def evaluate_patient(i, t_rad, t_treat_p1, t_treat_c4):
+  # Create a new random number generator with a unique seed for each patient
+  paramNew = params[i]
+  paramNew[22] = 0
+  paramNew[32] = 0
     #print(paramNew)
+  
   D = DList[i]
-  t_rad = schedule_list[i]
+  
+  # t_rad = np.array(schedule_list[i][0])
+  # #t_treat_c4 = np.zeros(3)
+  # #t_treat_p1 = np.zeros(3)
+  # t_treat_p1 = np.array(schedule_list[i][1])
+  # t_treat_c4 = np.array(schedule_list[i][2])
+  #t_f2 = max(schedule_list[i][0][0], schedule_list[i][1][0]) + 30
+  t_f2 = t_rad[-1] +30
+  #print('t_f2', t_f2)
+  # if not isinstance(t_f2, int):
+  #     t_f2 = t_f2[0]
+  
   vol, _, Time, _, C, *_ = radioimmuno_response_model(paramNew, delta_t, free, t_f1, t_f2, D, t_rad, t_treat_c4, t_treat_p1, LQL, activate_vd, use_Markov)
   #print(C)
-  #show_plot(time, vol)
-  if dp.getTreatmentTime(Time, C) != None:
-    treatmentTime = dp.getTreatmentTime(Time, C)
+  # plt.figure()
+  # plt.plot(Time, vol[0], color='red')
+  # plt.show()
+  # plt.close()
+  # plt.figure()
+  # plt.plot(Time, C[0], color='blue')
+  # plt.show()
+  # plt.close()
+  treatmentTime = dp.getTreatmentTime(Time, C)
+  if treatmentTime != None:
+    #print(paramNew)
+    #     plt.plot(Time, C[0])
+    #print(str(t_rad) + " " + str(C) + str(treatmentTime)) 
+    #print('time', treatmentTime)
     return treatmentTime
   else:
+    #print(Time[190:215])
+    #print(C[0][190:215])
+    #plt.figure()
+    #plt.plot(Time, C[0])
+    #plt.close()
     return np.nan
 
-def trial_treatment(i):
-  schedule = schedule_list[i]
-  t_rad = schedule
-  t_f2 = schedule[-1] + 30
+
+def trial_treatment(i, file):
+  t_rad = schedule_list[i][0]
+  # print('rad', t_rad)
+  t_treat_p1 = [10]
+  t_treat_c4 = [10]
+  # print('p1', t_treat_p1)
+  t_f2 = max(max(t_rad[-1], t_treat_p1[-1]), t_treat_c4[-1]) + 30
+  #print('trial t_f2', t_f2)
   treatment_times = []
   treatment_times_list = []
   D = DList[i]
-  args = [(i, k) for k in range(sample_size)]
+  args = [(j, t_rad, t_treat_p1, t_treat_c4) for j in range(sample_size)]
+  #print('args', args)
   with concurrent.futures.ThreadPoolExecutor() as executor:
           treatment_times = list(executor.map(lambda p: evaluate_patient(*p), args))
   treatment_times = [x for x in treatment_times if np.isnan(x) == False]
@@ -100,21 +147,31 @@ def trial_treatment(i):
       treatment_times = np.array(treatment_times)
       treatment_res_list = [t_rad, D, np.mean(treatment_times), np.mean(treatment_times) - t_rad[0], np.std(treatment_times), len(treatment_times)/sample_size, treatment_times]
   return treatment_res_list
-# Define the number of iterations
-iterations = len(schedule_list)  # Or any other number of iterations
-print(param)
+
+# Define the treatment schedules and doses
+schedules, DList = get_treatment_and_dose(60, RT_fractions, param, PD_fractions, CTLA4_fractions)
+#print(schedules)
+iterations = len(schedules)  # Or any other number of iterations
+#param_file = open('parameters.txt', 'w')
+# for k in range(min(iterations,50)):
+#     print('k', k)
+#     print(params[k])
+args = [(k, params) for k in range(min(iterations,1200))]
 # Use a ThreadPoolExecutor to run the iterations in parallel
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    data = list(executor.map(trial_treatment, range(iterations)))
+    data = list(executor.map(lambda p: trial_treatment(*p), args))
 
     #print(data)
     # Retrieve results from completed futures
 
 #print(data)
-dataFrame = pd.DataFrame(data, columns=["RT Treatment Days", "RT Dose", "Mean Treatment Time From Starting Tumour Size", "Mean Treatment Time After Treatment Started", "SD Treatment Time", "TCP", "List of Treatment Times"])
+dataFrame = pd.DataFrame(data, columns=["RT Treatment Days", "RT Dose (Gy)", "Mean Treatment Time From Starting Tumour Size", "Mean Treatment Time After Treatment Started", "SD Treatment Time", "TCP", "List of Treatment Times"])
 print(dataFrame)
 dataFrame.to_csv(file_name, index=False)
 end_time = time.time()
-f = open('time taken RT ' + str(num_fractions) + ' treatment eval.txt', 'w')
+f = open('time taken RT ' + str(RT_fractions) + ' treatment eval a constant seed.txt', 'w')
 f.write("TIME TAKEN " + str(end_time - start_time))
-f.close
+print("TIME TAKEN " + str(end_time - start_time))
+f.close()
+
+
